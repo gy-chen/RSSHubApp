2d1f40829e25cfa803303626869ef4fdb5a7216c
GYCHEN <gy.chen@gms.nutc.edu.tw>
2020-03-24 14:39:53 +0800

    support load rsshub from cloned rsshub repository

--- bin/RSSHubApp.js
+++ bin/RSSHubApp.js
@@ -2,10 +2,8 @@
 "use strict";
 
 const { utils, middlewares, localRSSHub } = require("../lib/");
-const rsshub = localRSSHub.requireRSSHub();
-rsshub.init();
-
 const app = localRSSHub.requireRSSHubApp();
+
 const routesMiddleware = utils.findRSSHubRoutesMiddleware(app);
 const customRouter = middlewares.createCustomRouter(routesMiddleware);
 utils.replaceRSSHubRoutesMiddleware(app, customRouter.routes());
